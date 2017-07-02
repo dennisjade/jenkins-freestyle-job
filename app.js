abcf876c21dfd4fb2fb4a2847f2971d1db1dd70f
Dencio <toribiod@sg.ibm.com>
2017-07-03 02:44:22 +0800

    2

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
+  //process.exit(-1);
 });
+
+process.exit(-1);
 
 module.exports = app;
